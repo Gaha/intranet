29571bf08ff831c35768dba6e255dfdd3f37c41e
CER5400020 <cer5400020@CER54-CVE-VDE-F.cve.recouv>
2014-02-28 15:24:05 +0100

    correction syntaxique

--- ce/urls.py
+++ ce/urls.py
@@ -3,9 +3,9 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('ce.views',
-    url(r'^$', 'membres_dup'),
-    url(r'^agent', 'view_agent', name = 'agent'),
-    url(r'^commission', 'view_commission', name = 'commission'),
-    url(r'^activitee', 'view_activitee', name = 'activitee'),
-    url(r'^participation', 'view_participation', name = 'participation'),
-    )
+                       url(r'^$', 'membres_dup'),
+                       url(r'^agent', 'view_agent', name='agent'),
+                       url(r'^commission', 'view_commission', name='commission'),
+                       url(r'^activitee', 'view_activitee', name='activitee'),
+                       url(r'^participation', 'view_participation', name='participation'),
+                       )
