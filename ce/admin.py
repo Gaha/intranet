f4b15dcd4630bbd51506198e76ef9282fe4df1ed
CER5400020 <cer5400020@CER54-CVE-VDE-F.cve.recouv>
2014-02-28 15:20:18 +0100

    corrections syntaxiques

--- ce/admin.py
+++ ce/admin.py
@@ -3,37 +3,43 @@
 from django.contrib import admin
 from ce.models import Agent, Commission, Activitee, Participation, Mendat, CommissionMembre
 
-class AgentAdmin(admin.ModelAdmin) :
-    list_display = ('nom','prenom','contrat')
-    list_filter = ('nom','prenom','contrat')
-    ordering = ('nom',)
-    search_fields = ('nom','prenom')
 
-class ActiviteeAdmin(admin.ModelAdmin) :
-    list_display = ('date','heure','commission','nom')
-    list_filter = ('date','commission','nom')
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'contrat')
+    list_filter = ('nom', 'prenom', 'contrat')
+    ordering = ('nom',)
+    search_fields = ('nom', 'prenom')
+
+
+class ActiviteeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'heure', 'commission', 'nom')
+    list_filter = ('date', 'commission', 'nom')
     date_hierarchy = ('date')
     ordering = ('date',)
-    search_fields = ('commission','nom')
-    
-class ParticipationAdmin(admin.ModelAdmin) :
-    list_display = ('nom','activitee','agent','enfant','conjoint','externe','etat')
-    list_filter = ('nom','activitee')
-    ordering = ('activitee','nom')
+    search_fields = ('commission', 'nom')
+
+
+class ParticipationAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'activitee', 'agent', 'enfant', 'conjoint', 'externe', 'etat')
+    list_filter = ('nom', 'activitee')
+    ordering = ('activitee', 'nom')
     search_fields = ('nom', 'activitee')
 
-class MendatAdmin(admin.ModelAdmin) :
-    list_display = ('nom','mendat')
+
+class MendatAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'mendat')
     list_filter = ('mendat',)
 
-class CommissionMembreAdmin(admin.ModelAdmin) :
-    list_display = ('commission','agent','membre')
-    list_filter = ('commission','membre')
+
+class CommissionMembreAdmin(admin.ModelAdmin):
+    list_display = ('commission', 'agent', 'membre')
+    list_filter = ('commission', 'membre')
     ordering = ('commission',)
+
 
 admin.site.register(Agent, AgentAdmin)
 admin.site.register(Commission)
 admin.site.register(Activitee, ActiviteeAdmin)
 admin.site.register(Participation, ParticipationAdmin)
 admin.site.register(Mendat, MendatAdmin)
-admin.site.register(CommissionMembre,CommissionMembreAdmin)
+admin.site.register(CommissionMembre, CommissionMembreAdmin)
