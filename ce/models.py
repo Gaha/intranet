5f31ea66519648053a319b229ffad099d3a66e93
CER5400020 <cer5400020@CER54-CVE-VDE-F.cve.recouv>
2014-02-28 15:17:27 +0100

    corrections syntaxiques

--- ce/models.py
+++ ce/models.py
@@ -5,83 +5,88 @@ from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class Agent(models.Model):
     LISTE_CONTRAT = (
-        ('CDI','CDI'),
-        ('CDD','CDD'),
-        ('PRE','Prestataire'),
-        ('INT','Intérime'),
+        ('CDI', 'CDI'),
+        ('CDD', 'CDD'),
+        ('PRE', 'Prestataire'),
+        ('INT', 'Intérime'),
     )
 
-    nom = models.CharField(max_length = 50)
-    prenom = models.CharField(max_length = 50)
-    contrat = models.CharField(max_length = 3, choices=LISTE_CONTRAT, default='CDI')
-    
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    contrat = models.CharField(max_length=3, choices=LISTE_CONTRAT, default='CDI')
+
     # c'est ce qui s'affiche quand on print, notament dans admin
-    def __unicode__(self) :
+    def __unicode__(self):
         return u'%s %s' % (self.nom, self.prenom)
 
-class Mendat(models.Model) :
+
+class Mendat(models.Model):
     LISTE_MENDAT = (
-        ('DUP_PR','DUP Président'),
-        ('DUP_SE','DUP Secrétaire'),
-        ('DUP_TR','DUP Trésorier'),
-        ('CA_TIT','DUP Cadre Titulaire'),
-        ('CA_SUP','DUP Cadre Suppléant'),
-        ('AG_TIT','DUP Agent Titulaire'),
-        ('AG_SUP','DUP Agent Suppléant'),
-        ('DS','Délégués Syndical'),
-        ('CHS_PR','CHSCT Président'),
-        ('CHS_SE','CHSCT Secrétaire'),
-        ('CHS_ME','CHSCT Membres'),
+        ('DUP_PR', 'DUP Président'),
+        ('DUP_SE', 'DUP Secrétaire'),
+        ('DUP_TR', 'DUP Trésorier'),
+        ('CA_TIT', 'DUP Cadre Titulaire'),
+        ('CA_SUP', 'DUP Cadre Suppléant'),
+        ('AG_TIT', 'DUP Agent Titulaire'),
+        ('AG_SUP', 'DUP Agent Suppléant'),
+        ('DS', 'Délégués Syndical'),
+        ('CHS_PR', 'CHSCT Président'),
+        ('CHS_SE', 'CHSCT Secrétaire'),
+        ('CHS_ME', 'CHSCT Membres'),
     )
     nom = models.ForeignKey('Agent')
-    mendat = models.CharField(max_length = 6, choices=LISTE_MENDAT)
+    mendat = models.CharField(max_length=6, choices=LISTE_MENDAT)
 
-    def __unicode__(self) :
-        return u'%s - %s' %(self.nom, self.mendat)
+    def __unicode__(self):
+        return u'%s - %s' % (self.nom, self.mendat)
+
 
 class Commission(models.Model):
-    nom = models.CharField(max_length = 50)
-    
-    def __unicode__(self) :
-        return u'%s' %(self.nom)
+    nom = models.CharField(max_length=50)
 
-class CommissionMembre(models.Model) :
+    def __unicode__(self):
+        return u'%s' % (self.nom)
+
+
+class CommissionMembre(models.Model):
     LISTE_MEMBRE = (
-        ('PRE','Président'),
-        ('DUP','Membre DUP'),
-        ('AGE','Membre Agent')
+        ('PRE', 'Président'),
+        ('DUP', 'Membre DUP'),
+        ('AGE', 'Membre Agent')
     )
     commission = models.ForeignKey('Commission')
     agent = models.ForeignKey('Agent')
-    membre = models.CharField(max_length = 3, choices=LISTE_MEMBRE)
+    membre = models.CharField(max_length=3, choices=LISTE_MEMBRE)
 
-    def __unicode__(self) :
-        return u'%s : %s - %s' %(self.commission, self.agent, self.membre)
+    def __unicode__(self):
+        return u'%s : %s - %s' % (self.commission, self.agent, self.membre)
 
 
 class Activitee(models.Model):
-    nom = models.CharField(max_length = 50)
+    nom = models.CharField(max_length=50)
     commission = models.ForeignKey('Commission')
     date = models.DateField()
     heure = models.TimeField()
-    
-    def __unicode__(self) :
+
+    def __unicode__(self):
         return u'%s : %s' % (self.date, self.nom)
 
-class Participation(models.Model) :
+
+class Participation(models.Model):
     LISTE_ETAT = (
-        ('IN','Inscrit'),
-        ('AN','Annuler'),
+        ('IN', 'Inscrit'),
+        ('AN', 'Annuler'),
     )
     nom = models.ForeignKey('Agent')
     activitee = models.ForeignKey('Activitee')
-    agent = models.IntegerField(default = 1)
-    conjoint = models.IntegerField(default = 0)
-    enfant = models.IntegerField(default = 0)
-    externe = models.IntegerField(default = 0)
-    etat = models.CharField(max_length = 2, choices=LISTE_ETAT, default='IN')
-    
+    agent = models.IntegerField(default=1)
+    conjoint = models.IntegerField(default=0)
+    enfant = models.IntegerField(default=0)
+    externe = models.IntegerField(default=0)
+    etat = models.CharField(max_length=2, choices=LISTE_ETAT, default='IN')
+
     def __unicode__(self):
         return u'%s : %s' % (self.activitee, self.nom)
